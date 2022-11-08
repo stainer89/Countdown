42b4758b94520f037790d0f0fe017ca85ae5c7e1
Charles Stainer <cstainer@gmu.edu>
2022-11-08 12:33:30 -0500

    Initial Commit

---
+++ Shared/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Shared
+//
+//  Created by Charles Stainer on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
